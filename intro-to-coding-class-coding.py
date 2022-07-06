7adf64577b0fbeb8a6aa89f63debd6d17f78931a
James Miller <jammiller.mba@gmail.com>
2022-07-06 12:56:21 -0400

    first sync

---
+++ intro-to-coding-class-coding.py
@@ -0,0 +1,48 @@
+# Exercise: Intro to Classes - Class Coding
+
+class Animal:
+    def __init__(self, species, aclass, order):
+        self.species = species
+        self.aclass = aclass
+        self.order = order
+
+    def eat():
+        "eat something"
+    
+    def reproduce():
+        "Have offspring"
+    
+    def sleep():
+        "Go to sleep"
+
+class Book:
+    def __init__(self, title, author, publisher, published_date, pages, non_fiction, genre):
+        self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.published_date = published_date
+        self.pages = pages
+        self.non_fiction = non_fiction
+        self.genre = genre
+
+    def open():
+        "open the book"
+    
+    def turnPage():
+        "turn to the next page"
+
+class Vehicle:
+    def __init__(self, make, model, year, engine, seats):
+        self.title = title
+        self.make = make
+        self.model = model
+        self.year = year
+        self.engine = engine
+        self.non_fiction = non_fiction
+        self.seats = seats
+
+    def drive():
+        "drive the car"
+    
+    def park():
+        "park the car"
